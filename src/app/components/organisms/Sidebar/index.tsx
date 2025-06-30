0a330375f3b11e5df56397cd902e257f108d90eb
unknown <fredy.hoyos1@udea.edu.co>
2025-06-29 20:20:37 -0500

    feat: Se crea el layout y se crea las carpetas iniciales

---
+++ src/app/components/organisms/Sidebar/index.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+
+const Sidebar = () => {
+  const userRole = 'admin'; // Simula el rol
+
+  return (
+    <div>
+      <aside className="w-64 h-screen bg-gray-900 text-white p-4 fixed left-0 top-0">
+        <h2 className="text-xl font-bold mb-4">Menú</h2>
+        <ul>
+          <li className="mb-2">
+            <Link href="/transitions" className="hover:underline">Transacciones</Link>
+          </li>
+          <li className="mb-2">
+            <Link href="/masterful" className="hover:underline">Maestros</Link>
+          </li>
+
+          {/* Solo los admin pueden ver este enlace */}
+          {userRole === 'admin' && (
+            <li className="mb-2">
+              <Link href="/users" className="hover:underline">Usuarios</Link>
+            </li>
+          )}
+        </ul>
+      </aside>
+    </div>
+  );
+};
+
+export default Sidebar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return (
-    <div>Home</div>
-  );
+  redirect('/dashboard');
 }
