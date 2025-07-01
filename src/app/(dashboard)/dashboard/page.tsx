d90aa8d112c8ad12f07eeb46f4eb745192edc63c
unknown <fredy.hoyos1@udea.edu.co>
2025-06-30 22:14:14 -0500

    feat: Correccion error 404

---
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import Sidebar from "@/app/components/organisms/Sidebar";
+
+export default function DashboardHome() {
+    return (
+      <div>
+        
+        <Sidebar />
+      </div>
+    );
+  }
+  
