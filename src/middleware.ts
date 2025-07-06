6ff7385616b02098d28c4bd082b8d1b63280f469
mateo.velasquez <mateo.velasquez@ServiciosNutresa.onmicrosoft.com>
2025-07-06 17:32:34 -0500

    feat: middleware actualice

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,22 @@
-export { default } from "next-auth/middleware"
+import { NextRequest, NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
 
-export const config = { matcher: ["/dashboard"] }
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  const isAuthPage = req.nextUrl.pathname.startsWith('/auth/Login') || req.nextUrl.pathname.startsWith('/auth');
+
+  if (!token && !isAuthPage) {
+    const url = req.nextUrl.clone();
+    url.pathname = '/auth/Login';
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    '/dashboard/:path*'
+  ],
+};
