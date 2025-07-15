53a2ca5401aef35b7eef211acffcc88633840ce2
Douglas-Lanny <douglas.alarcon@udea.edu.co>
2025-07-15 18:27:27 -0500

    conexion a datos de usuario para el get

--- src/app/(dashboard)/users/page.tsx
+++ src/app/(dashboard)/users/page.tsx
@@ -3,19 +3,33 @@ import React from 'react'
 import { Users } from 'lucide-react';
 import HeadingBlock from '@/components/molecules/HeadingBlock';
 import UsersTable from '@/components/organisms/UsersTable';
+import { prisma } from '@/libs/prisma';
 
-const page = () => {
+
+
+async function loadUsers() {
+
+  return await prisma.user.findMany();
+
+}
+
+const page = async () => {
+
+  const users = await loadUsers().then(users => 
+    users.map(user => ({
+      ...user,
+      createdDate: user.createdAt.toISOString(),
+    }))
+  );
   return (
     <div className='flex flex-col gap-6'>
-
-
 
       <div className="flex flex-row justify-between items-center">
         <HeadingBlock title={'Usuarios'} description={'Administra usuarios y roles del sistema'} />
         <Users size={30} className='text-secondary' />
       </div>
 
-    <UsersTable />
+    <UsersTable users={users} />
     
     </div>
   )
--- src/components/organisms/UsersTable/index.tsx
+++ src/components/organisms/UsersTable/index.tsx
@@ -1,6 +1,21 @@
 import React from 'react'
 
-const Index = () => {
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  role: string;
+  createdDate: string;
+}
+
+interface UsersTableProps {
+  users: User[];
+}
+
+
+const Index = ({ users }: UsersTableProps) => {
+
   return (
 
     <div className='card flex flex-col gap-5 '>
@@ -8,49 +23,49 @@ const Index = () => {
         Lista de Usuarios
       </h3>
 
-      <div className="max-h-90 overflow-y-auto">
-        <table className="table-base">
-          <thead>
-            <tr>
-              <th className="table-header-cell">Usuario</th>
-              <th className="table-header-cell">Correo Electrónico</th>
-              <th className="table-header-cell">Rol</th>
-              <th className="table-header-cell">Fecha Creación</th>
-              <th className="table-header-cell">Acciones</th>
-            </tr>
-          </thead>
-          <tbody className="bg-white dark:bg-white">
-            <tr className="border-b table-row">
-              <td className="table-cell">Juan Pérez</td>
-              <td className="table-cell">juan.perez@email.com</td>
-              <td className="table-cell table-cell-role-admin">ADMIN</td>
-              <td className="table-cell table-cell-muted">2025-07-01</td>
-              <td className="table-cell">
-                <button className="table-action-button">Editar</button>
-              </td>
-            </tr>
-            <tr className="border-b table-row">
-              <td className="table-cell">Ana Gómez</td>
-              <td className="table-cell">ana.gomez@email.com</td>
-              <td className="table-cell table-cell-role-user">USER</td>
-              <td className="table-cell table-cell-muted">2025-06-15</td>
-              <td className="table-cell">
-                <button className="table-action-button">Editar</button>
-              </td>
-            </tr>
-            <tr className="border-b table-row">
-              <td className="table-cell">Carlos Ruiz</td>
-              <td className="table-cell">carlos.ruiz@email.com</td>
-              <td className="table-cell table-cell-role-user">USER</td>
-              <td className="table-cell table-cell-muted">2025-05-20</td>
-              <td className="table-cell">
-                <button className="table-action-button">Editar</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      {users.length > 0 ?
+        (
+          <div className="max-h-90 overflow-y-auto">
+            <table className="table-base">
+              <thead>
+                <tr>
+                  <th className="table-header-cell">Usuario</th>
+                  <th className="table-header-cell">Correo Electrónico</th>
+                  <th className="table-header-cell">Rol</th>
+                  <th className="table-header-cell">Fecha Creación</th>
+                  <th className="table-header-cell">Acciones</th>
+                </tr>
+              </thead>
+              <tbody >
+
+                {users.map((user, index) => (
+
+                  <tr className="border-b table-row" key={user.id}>
+                    <td className="table-cell">{user.name}</td>
+                    <td className="table-cell">{user.email}</td>
+                    <td className="table-cell table-cell-role-admin">{user.role}</td>
+                    <td className="table-cell table-cell-muted">{user.createdDate}</td>
+                    <td className="table-cell">
+
+                      {index > 0 && ( <button className="table-action-button">Editar</button>) }
+                    </td>
+                  </tr>
+
+                ))}
+
+              </tbody>
+            </table>
+          </div>
+
+        )
+        :
+        (
+          <p className="text-gray-500">No hay maestros registrados.</p>
+        )
+      }
+
     </div>
+
   )
 }
 
