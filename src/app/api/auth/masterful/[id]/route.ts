13d17c4a467991d9333d311728393fe8d16bc2a1
mateo.velasquez <mateo.velasquez@ServiciosNutresa.onmicrosoft.com>
2025-07-17 21:59:27 -0500

    feat: prueba

--- src/app/api/auth/masterful/[id]/route.ts
+++ src/app/api/auth/masterful/[id]/route.ts
@@ -1,42 +1,34 @@
 import { NextResponse } from "next/server";
-import { NextRequest } from 'next/server';
 import { prisma } from '@/libs/prisma';
-import { getServerSession } from 'next-auth/next';
-import { authOptions } from '../../../../../libs/auth0ptions';
 
-interface Params {
-  id: string;
+
+// PUT: Actualiza un maestro
+export async function PUT(request: Request, { params }: { params: { id: string } }) {
+  const { id } = await params;
+  const data = await request.json();
+
+  const updatedMaster = await prisma.master.update({
+    where: { id: parseInt(id) },
+    data: {
+      name: data.name,
+      balance: parseInt(data.balance),
+    },
+  });
+
+  return NextResponse.json(updatedMaster, {
+    status: 200, // Código de estado correcto para PUT
+  });
 }
 
-export async function PUT(request: NextRequest, { params }: { params: Params }) {
-    const { id } = params;
-    const data = await request.json();
-    const session = await getServerSession(authOptions);
-    const updatedMaster = await prisma.master.update({
-        where: { id: parseInt(id) },
-        data: {
-            name: data.name,
-            balance: parseInt(data.balance),
-            creator: {
-                connect: {
-                    id: parseInt(session?.user.id)
-                }
-            }
-        }
-    });
+// DELETE: Elimina un maestro
+export async function DELETE(request: Request, { params }: { params: { id: string } }) {
+  const { id } = await params;
 
-    return NextResponse.json(updatedMaster, {
-        status: 201,
-    });
-}
-
-export async function DELETE(request: NextRequest, { params }: { params: Params }) {
-  const { id } = params;
-  const deletionMaster = await prisma.master.delete({
+  const deletedMaster = await prisma.master.delete({
     where: { id: parseInt(id) },
   });
 
-  return NextResponse.json(deletionMaster, {
-    status: 201,
+  return NextResponse.json(deletedMaster, {
+    status: 200, // Código de estado correcto para DELETE
   });
 }
