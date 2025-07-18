bf197fdf14a2d164b77013ad479df0473f4be4d1
Douglas-Lanny <douglas.alarcon@udea.edu.co>
2025-07-18 00:42:38 -0500

    correccion error de tipos

--- src/app/api/auth/masterful/[id]/route.ts
+++ src/app/api/auth/masterful/[id]/route.ts
@@ -3,8 +3,8 @@ import { prisma } from '@/libs/prisma';
 
 
 // PUT: Actualiza un maestro
-export async function PUT(request: Request, context: { params: { id: string } }) {
-  const { id } = context.params;
+export async function PUT(request: Request, context: { params: Promise<{ id: string }> }) {
+  const { id } = await context.params;
   const data = await request.json();
 
   const updatedMaster = await prisma.master.update({
@@ -21,8 +21,8 @@ export async function PUT(request: Request, context: { params: { id: string } })
 }
 
 // DELETE: Elimina un maestro
-export async function DELETE(request: Request, context: { params: { id: string } }) {
-  const { id } = context.params;
+export async function DELETE(request: Request, context: { params: Promise<{ id: string }> }) {
+  const { id } = await context.params;
 
   const deletedMaster = await prisma.master.delete({
     where: { id: parseInt(id) },
